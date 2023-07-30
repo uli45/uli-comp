01855fc38c1f04a60e510dc231bc246333ecfac7
uli45 <1252670924@qq.com>
2023-07-30 18:54:13 +0800

    添加打包命令和自动发布

---
+++ packages/components/src/icon/icon.vue
@@ -0,0 +1,8 @@
+
+<script lang="ts" setup>
+import './style/index.less';
+defineOptions({ name: "uli-icon" });
+</script>
+<template>
+    <div class="uli-icon">Icon1</div>
+</template>
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     //minify: false,
     rollupOptions: {
       //忽略打包vue文件
-      external: ["vue"],
+      external: ["vue", /\.less/],
       input: ["index.ts"],
       output: [
         {
@@ -37,25 +37,45 @@ export default defineConfig({
           dir: "../uli-comp/lib",
         },
       ],
-      
     },
     lib: {
       entry: "./index.ts",
     },
-    cssCodeSplit: true,//是否将CSS拆分为更小的块并独立加载
-    cssTarget: 'chrome61' //防止 vite 将 rgba() 颜色转化为 #RGBA 十六进制符号的形式  (要兼容的场景是安卓微信中的 webview 时,它不支持 CSS 中的 #RGBA 十六进制颜色符号)
-    
+    cssCodeSplit: true, //是否将CSS拆分为更小的块并独立加载
+    cssTarget: "chrome61", //防止 vite 将 rgba() 颜色转化为 #RGBA 十六进制符号的形式  (要兼容的场景是安卓微信中的 webview 时,它不支持 CSS 中的 #RGBA 十六进制颜色符号)
   },
-  plugins: [vue(),dts({
-    entryRoot: "./src",
-    outputDir: ["../uli-comp/es/src", "../uli-comp/lib/src"],
-    //指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
-    tsConfigFilePath: "../../tsconfig.json",
-  }), DefineOptions()],
-  esbuild:{
-    drop: ['console','debugger'], // 删除 所有的console 和 debugger 包括console.log、console.warn、console.error等。
-    //如下配置只删除 debugger console.log  
+  plugins: [
+    vue(),
+    dts({
+      entryRoot: "./src",
+      outputDir: ["../uli-comp/es/src", "../uli-comp/lib/src"],
+      //指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
+      tsConfigFilePath: "../../tsconfig.json",
+    }),
+    {
+      name: "style",
+      generateBundle(config, bundle) {
+        //这里可以获取打包后的文件目录以及代码code
+        const keys = Object.keys(bundle);
+
+        for (const key of keys) {
+          const bundler: any = bundle[key as any];
+          //rollup内置方法,将所有输出文件code中的.less换成.css,因为我们当时没有打包less文件
+
+          this.emitFile({
+            type: "asset",
+            fileName: key, //文件名名不变
+            source: bundler.code.replace(/\.less/g, ".css"),
+          });
+        }
+      },
+    },
+    DefineOptions(),
+  ],
+  esbuild: {
+    drop: ["console", "debugger"], // 删除 所有的console 和 debugger 包括console.log、console.warn、console.error等。
+    //如下配置只删除 debugger console.log
     // pure: ['console.log'], // 删除 console.log
     // drop: ['debugger'], // 删除 debugger
-}
+  },
 });
